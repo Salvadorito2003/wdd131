5059479f20a45ab2f9b8b5779b609576cb0f68b7
Tobias <tobiasaguilartarea@gmail.com>
2025-07-14 17:01:48 -0300

    Fixed js

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,22 +1,28 @@
 const input = document.querySelector("#favchap");
 const button = document.querySelector("#submit");
 const list = document.querySelector("#list");
-const li = document.createElement("li");
-const deleteButton = document.createElement("button");
+
 
 button.addEventListener("click", function(){
-    if (input.value.trim() == '') {
-        print("Empty field.")
-    } else {
+    if (input.value.trim() !== '') {
+        const li = document.createElement("li");
+        const deleteButton = document.createElement("button");
+
         li.textContent = input.value;
-        deleteButton.textContent = "X";
+        deleteButton.textContent = "❌";
         li.append(deleteButton);
         list.append(li);
+
+        deleteButton.addEventListener("click", function(){
+        list.removeChild(li);
+        input.focus();
+        })
+
+        input.value = "";
+        input.focus();
+    } else {
+        console.log("Empty field.")
     }
+    
 });
-deleteButton.addEventListener("click", function(){
-    list.removeChild(li);
-    input.focus();
-})
-input.value = "";
-input.focus();
+
