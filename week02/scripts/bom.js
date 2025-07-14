859a5b4fb58010b8c89ae0f48e40a654282ade35
Tobias <tobiasaguilartarea@gmail.com>
2025-07-14 16:51:33 -0300

    Added functionality to js document

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -3,7 +3,20 @@ const button = document.querySelector("#submit");
 const list = document.querySelector("#list");
 const li = document.createElement("li");
 const deleteButton = document.createElement("button");
-li.textContent = input.value;
-deleteButton.textContent = "X";
-li.append(deleteButton);
-list.append(li);
+
+button.addEventListener("click", function(){
+    if (input.value.trim() == '') {
+        print("Empty field.")
+    } else {
+        li.textContent = input.value;
+        deleteButton.textContent = "X";
+        li.append(deleteButton);
+        list.append(li);
+    }
+});
+deleteButton.addEventListener("click", function(){
+    list.removeChild(li);
+    input.focus();
+})
+input.value = "";
+input.focus();
