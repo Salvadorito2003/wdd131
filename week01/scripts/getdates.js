feb8a9e53a057e0733096a152e3fc0a735800881
Tobias <tobiasaguilartarea@gmail.com>
2025-09-03 18:50:19 -0300

    Emptied Folder to start new block

--- week01/scripts/getdates.js
+++
@@ -1,6 +0,0 @@
-const yearElement = document.querySelector("#currentyear");
-const currentYear = new Date().getFullYear();
-yearElement.textContent = `© ${currentYear}`;
-
-const lastModifiedElement = document.querySelector("#lastmodified");
-lastModifiedElement.textContent = `Last Modified: ${document.lastModified}`;
