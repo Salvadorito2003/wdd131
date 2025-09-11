cce9808edaa455ded4e7ba9023d88f034468f93f
Tobias <tobiasaguilartarea@gmail.com>
2025-09-11 19:07:37 -0300

    I forgot to commit learning activities before - And now temples

---
+++ scripts/bom.js
@@ -0,0 +1,31 @@
+const input = document.querySelector("#favchapter");
+const button = document.querySelector("#submit");
+const list = document.querySelector("#list");
+
+
+button.addEventListener("click", function(){
+    if (input.value.trim() !== '') {
+        const li = document.createElement("li");
+        const deleteButton = document.createElement("button");
+
+
+        li.textContent = input.value;
+        deleteButton.textContent = "❌";
+        li.append(deleteButton);
+        list.append(li);
+
+
+        deleteButton.addEventListener("click", function(){
+        list.removeChild(li);
+        input.focus();
+        })
+
+
+        input.value = "";
+        input.focus();
+    } else {
+        console.log("Empty.")
+    }
+    
+});
+
