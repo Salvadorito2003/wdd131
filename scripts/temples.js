d272e85b6e108c882971a4282fece2a6266734d5
Tobias <tobiasaguilartarea@gmail.com>
2025-09-11 21:45:58 -0300

    Fixed colors

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,13 +1,16 @@
+
 const menuButton = document.querySelector("#menu");
 const navigation = document.querySelector(".navigation")
 const yearElement = document.querySelector("#currentyear");
 const currentYear = new Date().getFullYear();
 yearElement.textContent = `© ${currentYear}`;
 
+
 menuButton.addEventListener("click", () => {
 	navigation.classList.toggle("open");
 	menuButton.classList.toggle("open");
 });
+
 
 const lastModifiedElement = document.querySelector("#lastmodified");
 lastModifiedElement.textContent = `Last Modified: ${document.lastModified}`;
